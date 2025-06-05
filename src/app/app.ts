8962c64c0e5448108d836b640c41459393d113fd
THINKPAD_01\jerem <jeremygoheh@gmail.com>
2025-06-05 10:19:13 +0800

    Add functionality to update reading count and order

--- src/app/app.ts
+++ src/app/app.ts
@@ -27,9 +27,26 @@ class reading {
 @Component({
   selector: 'app-readings',
   template: `
+  <div>
+    <label for="readingsCount">Number of readings:</label>
+    <input type = "number" id = "readingsCount">
+</div>
+<br>
+<div>
+<label for="startFromTop">Order of readings:</label>
+<br>
+<input type = "radio" name = "startFromTop" checked (change) = "startsFromTop.set(true)">
+<label for = "startFromTop">Ascending</label>
+<input type = "radio" name = "startFromTop" checked (change) = "startsFromTop.set(false)">
+<label for = "startFromTop">Descending</label>
+</div>
+<br>
+  <div>
     <button (click) = "getReadings()">Get readings</button>
-    <br>
-    <table>
+  </div>
+  <br>
+
+  <table>
     <tbody>
       <tr>
       <th>Time</th>
@@ -58,11 +75,21 @@ class reading {
 export class ReadingsList {
   private http = inject(HttpClient);
   readings: Array<reading> = [];
+  private readingsCount = 0;
+  startsFromTop = signal(true);
 
+  // Function to fetch readings from the backend
   getReadings() {
     this.readings = [];
-    this.http.get<Array<reading>>('http://localhost:8080/readings')
-        .subscribe((subscriber) => {
+    let element: HTMLInputElement = document.getElementById("readingsCount") as HTMLInputElement;
+    if (element.value == "") {
+      // Default to 10 readings if no input is provided
+      this.readingsCount = 10;
+    } else {
+      this.readingsCount = parseInt(element.value);
+    }
+    this.http.get<Array<reading>>("http://localhost:8080/readings?count=" + this.readingsCount
+      + "&startsFromTop=" + this.startsFromTop()).subscribe((subscriber) => {
           for (let i = 0; i < subscriber.length; i++) {
             let new_reading = new reading(subscriber[i].time, subscriber[i].temperature, 
               subscriber[i].pressure, subscriber[i].humidity, subscriber[i].co2Ppm, subscriber[i].tvoc);  
